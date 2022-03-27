db80cb6750f5e77570bf922cf7ba931f597ec0b2
trippjoe <tripp.j@hotmail.com>
2022-03-27 13:58:59 -0400

    cleaned up file structure

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, '/index.html'));
+  mainWindow.loadFile(path.join(__dirname, '/html/index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
