8c3d65171a214b7e946e9f79e28d70b2b5571cdf
Tyler Wilding <xtvaser@gmail.com>
2022-06-01 22:28:13 -0400

    wire up uninstall button properly

--- src/components/games/Jak1Main.svelte
+++ src/components/games/Jak1Main.svelte
@@ -1,7 +1,7 @@
 <script>
   import { setInstallStatus } from "$lib/config";
   import { SUPPORTED_GAME } from "$lib/constants";
-
+  import { navigate } from "svelte-routing";
   import { launchGame } from "$lib/launch";
 
   function onClickConfig() {}
@@ -11,8 +11,8 @@
   }
 
   async function onClickUninstall() {
-    // TODO - refresh the UI
     await setInstallStatus(SUPPORTED_GAME.Jak1, false);
+    navigate("/jak1", { replace: true });
   }
 </script>
 
