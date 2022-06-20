0fafd342209a7827346fd99c8b7d93a4f1faba4f
Tyler Wilding <xtvaser@gmail.com>
2022-06-19 22:19:46 -0400

    splash: update splash to show some feedback about what's going on

--- src/splash/Splash.svelte
+++ src/splash/Splash.svelte
@@ -3,7 +3,12 @@
   import { areRequirementsMet, initConfig } from "$lib/config";
   import { checkRequirements } from "$lib/setup";
   import { onMount } from "svelte";
+  import logo from "$assets/images/logo.png";
   import "./splash.css";
+import { copyDataDirectory, isDataDirectoryUpToDate } from "$lib/utils/file";
+
+  let dataFilesCopied = false;
+  let unableToCopy = false;
 
   // Events
   onMount(async () => {
@@ -11,6 +16,33 @@
     if (!(await areRequirementsMet())) {
       await checkRequirements();
     }
-    await closeSplashScreen();
+    // See if we've copied the files to the AppDir yet
+    dataFilesCopied = await isDataDirectoryUpToDate();
+    if (dataFilesCopied) {
+      // Lies!
+      setTimeout(async function() { await closeSplashScreen() },2500);
+    } else {
+      // Copy the files
+      let ok = await copyDataDirectory();
+      if (!ok) {
+        unableToCopy = true;
+      } else {
+        await closeSplashScreen();
+      }
+    }
   });
 </script>
+
+<div class="splash-logo">
+  <img src={logo}>
+</div>
+<div class="splash-status">
+  {#if !dataFilesCopied}
+    Copying Data Files...
+    {#if unableToCopy}
+    Error - Unable to Copy Data Files
+    {/if}
+  {:else}
+  Checking Data Files...
+  {/if}
+</div>
