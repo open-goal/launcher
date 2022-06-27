e1c7cc55573c81a3a4aeff4e4eb1a1f3cbd39f8a
trippjoe <tripp.j@hotmail.com>
2022-06-27 14:23:14 -0400

    added version numbers to settings

--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -2,6 +2,9 @@
   import { fade } from "svelte/transition";
   import { openDir } from "$lib/commands";
   import { appDir } from "@tauri-apps/api/path";
+  import { getGameInstallVersion } from "$lib/config";
+  import { SupportedGame } from "$lib/constants";
+  import { getVersion } from "@tauri-apps/api/app";
 
   // NOTE: added this to shut up the console warning
   export let location;
@@ -15,5 +18,11 @@
       <i class="bi bi-folder" />
       Open App Directory</button
     >
+  {/await}
+  {#await getGameInstallVersion(SupportedGame.Jak1) then version}
+    <p>Game Version: {version}</p>
+  {/await}
+  {#await getVersion() then version}
+    <p>App Version: {version}</p>
   {/await}
 </div>
