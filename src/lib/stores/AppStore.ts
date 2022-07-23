3f09a0048fd1b88b8d493ebb00e7565178b5b61c
Tyler Wilding <xtvaser@gmail.com>
2022-07-23 17:02:16 -0400

    setup: fix individually decompile/compile steps

--- src/lib/stores/AppStore.ts
+++ src/lib/stores/AppStore.ts
@@ -5,5 +5,7 @@ export const InstallStatus = writable({
   percent: undefined,
 });
 export const isInstalling = writable(false);
+export const isDecompiling = writable(false);
+export const isCompiling = writable(false);
 export const gameNeedsReinstall = writable(false);
 export const ProcessLogs = writable("");
