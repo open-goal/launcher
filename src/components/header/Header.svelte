2246fab981f3c894acf7b98335b7876016dae050
tripp <86533397+trippjoe@users.noreply.github.com>
2022-09-09 08:56:03 -0400

    reduced top window bar height

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -17,17 +17,17 @@
 </script>
 
 <header
-  class="flex flex-row h-12 bg-[#101010] pl-2 pr-4 items-center"
+  class="flex flex-row h-10 bg-[#101010] pl-2 pr-4 items-center"
   data-tauri-drag-region
 >
   <div class="flex flex-row items-center space-x-2 text-2xl">
-    <img class="h-10" src={icon} alt="" />
+    <img class="h-8" src={icon} alt="" />
     <p>OpenGOAL</p>
   </div>
 
   <div class="border-l border-[#9f9f9f] h-10 m-2" />
 
-  <div class="flex flex-col space-y-0 text-[#9f9f9f]">
+  <div class="flex flex-col text-[#9f9f9f]">
     <p>v{launcherVerison} Launcher</p>
     <p>{gameVersion} Tools</p>
   </div>
