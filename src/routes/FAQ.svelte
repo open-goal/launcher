740063fb84dc733f2ab1e6c796497465e81dd737
tripp <86533397+trippjoe@users.noreply.github.com>
2022-09-09 12:17:29 -0400

    settings and faq route styling

--- src/routes/FAQ.svelte
+++ src/routes/FAQ.svelte
@@ -1,1 +1,45 @@
-FAQ --------- TODO
+<script>
+  import { Alert, Button } from "flowbite-svelte";
+</script>
+
+<div class="ml-20">
+  <div class="flex flex-col h-5/6 p-8">
+    <Alert color="dark" rounded={false}>
+      <span class="text-lg font-medium text-blue-700 dark:text-blue-800"
+        >Help & Support</span
+      >
+      <div slot="extra">
+        <div class="mt-2 mb-4 text-sm text-blue-700 dark:text-blue-800">
+          TODO ---- Fill this out with some COOL text about HELPING people WITH
+          THE GAME.
+        </div>
+        <div class="flex flex-col gap-2">
+          <Button
+            href="https://github.com/open-goal/launcher/issues"
+            target="_blank"
+            rel="noreferrer noopener"
+            size="md"
+            outline
+            color="blue">Report Launcher Issue</Button
+          >
+          <Button
+            href="https://github.com/open-goal/jak-project/issues"
+            target="_blank"
+            rel="noreferrer noopener"
+            size="md"
+            outline
+            color="blue">Report Game Issue</Button
+          >
+          <Button
+            href="https://discord.gg/VZbXMHXzWv"
+            target="_blank"
+            rel="noreferrer noopener"
+            size="md"
+            outline
+            color="blue">Discord</Button
+          >
+        </div>
+      </div>
+    </Alert>
+  </div>
+</div>
--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -1,16 +1,45 @@
 <script>
-  import { fade } from "svelte/transition";
   import { openDir } from "$lib/rpc/commands";
   import { appDir } from "@tauri-apps/api/path";
+  import { Alert, Button } from "flowbite-svelte";
+  import { onMount } from "svelte";
+
+  let directory = undefined;
+
+  onMount(async () => {
+    directory = await appDir();
+  });
 </script>
 
-<!-- TODO - STYLE THIS PAGE -->
-<div class="flex-center" in:fade>
-  <h1>Settings</h1>
-  {#await appDir() then directory}
-    <button class="btn" on:click={() => openDir(directory)}>
-      <i class="fa fa-folder" />
-      Open App Directory</button
-    >
-  {/await}
+<div class="ml-20">
+  <div class="flex flex-col h-5/6 p-8">
+    <Alert color="dark" rounded={false}>
+      <span class="text-lg font-medium text-black-900 dark:text-blue-800 mb-4"
+        >Settings</span
+      >
+      <div slot="extra">
+        <div class="flex flex-col gap-2">
+          <Button
+            size="md"
+            outline
+            color="green"
+            on:click={() => openDir(directory)}
+          >
+            <i class="fa fa-folder mx-1" />
+            Open Launcher Install Directory
+          </Button>
+          <Button
+            size="md"
+            outline
+            color="green"
+            on:click={() => openDir(directory)}
+          >
+            <i class="fa fa-folder mx-1" />
+            Open Logs Directory
+          </Button>
+          <!-- TODO: Add a button that lets users change the install directory -->
+        </div>
+      </div>
+    </Alert>
+  </div>
 </div>
