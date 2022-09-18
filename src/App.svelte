6bacb63b156ecc7da1fdee3ce3b76aceb27701b0
tripp <86533397+trippjoe@users.noreply.github.com>
2022-09-17 22:06:08 -0400

    Use better poster background image

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   // Assets
   import bgVideo from "$assets/videos/background.mp4";
-  import bgVideoPoster from "$assets/images/background-jak1-fallback.webp";
+  import bgVideoPoster from "$assets/backgrounds/village1.png";
   // Other Imports
   import { onMount } from "svelte";
   import { Router, Route } from "svelte-navigator";
