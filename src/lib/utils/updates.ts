15503b2dc9ac5768be04f9937e0c8ec391826d5e
tripp <86533397+trippjoe@users.noreply.github.com>
2022-09-18 18:34:06 -0400

    connect the update notes to the front end

--- src/lib/utils/updates.ts
+++ src/lib/utils/updates.ts
@@ -4,10 +4,12 @@ import { UpdateStore } from '$lib/stores/AppStore';
 export async function handleCheckUpdate() {
   const { shouldUpdate, manifest } = await checkUpdate();
   if (shouldUpdate) {
-    UpdateStore.update((UpdateStore) => UpdateStore = { shouldUpdate, manifest });
+    const { changes } = JSON.parse(manifest.body);
+    const { jak_project, launcher } = changes;
+    UpdateStore.update((UpdateStore) => UpdateStore = { shouldUpdate, jak_project, launcher });
     console.log('Update Available');
   } else {
-    UpdateStore.set({ shouldUpdate: false, manifest: {} });
+    UpdateStore.update((UpdateStore) => UpdateStore.shouldUpdate = shouldUpdate);
     console.log('Up to date');
   }
 }
--- src/routes/Update.svelte
+++ src/routes/Update.svelte
@@ -1,36 +1,75 @@
 <script>
   import { installUpdate } from "@tauri-apps/api/updater";
   import { relaunch } from "@tauri-apps/api/process";
-  import { Alert, Button } from "flowbite-svelte";
+  import {
+    Button,
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+  } from "flowbite-svelte";
   import { UpdateStore } from "$lib/stores/AppStore";
 
   let disabled = false;
+  let notesBool = true;
 
   async function updateHandler() {
     disabled = true;
     await installUpdate();
     await relaunch();
   }
+
+  $: jakProjectNotes = $UpdateStore?.jak_project;
+  $: launcherNotes = $UpdateStore?.launcher;
 </script>
 
 <div class="ml-20">
-  <div class="flex flex-col h-5/6 p-8">
+  <div class="flex flex-col h-[560px] max-h-[560px] p-8 gap-2">
     {#if $UpdateStore.shouldUpdate}
-      <Alert color="dark" rounded={false} accent>
-        <span class="text-2xl font-bold text-black-900">Update Available</span>
-        <div slot="extra">
-          <div class="mt-2 mb-4 text-lg text-blue-700">
-            <p>{$UpdateStore.manifest.body}</p>
-          </div>
-          <div class="flex flex-col">
-            <Button
-              class="!rounded-none !bg-[#222222] border-none !text-white hover:!text-yellow-300 !text-2xl"
-              on:click={async () => await updateHandler()}
-              {disabled}>Update</Button
-            >
-          </div>
-        </div>
-      </Alert>
+      <Table hoverable={true}>
+        <caption
+          class="p-2 font-semibold text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800"
+        >
+          <Button
+            class="!rounded-none w-48"
+            on:click={() => (notesBool = !notesBool)}
+            >Read {notesBool ? "Launcher" : "Jak-Project"} updates</Button
+          >
+        </caption>
+        <TableHead>
+          <TableHeadCell>Contributor</TableHeadCell>
+          <TableHeadCell>Description</TableHeadCell>
+          <TableHeadCell>Pull Request</TableHeadCell>
+        </TableHead>
+        <TableBody class="divide-y">
+          {#each notesBool ? jakProjectNotes : launcherNotes as note}
+            <TableBodyRow>
+              <TableBodyCell>{note.contributor}</TableBodyCell>
+              <TableBodyCell tdClass="overflow-clip"
+                >{note.description}</TableBodyCell
+              >
+              <TableBodyCell>
+                <Button
+                  class="!rounded-none"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  outline
+                  href={note.pullRequestUrl}>Github</Button
+                >
+              </TableBodyCell>
+            </TableBodyRow>
+          {/each}
+        </TableBody>
+      </Table>
+      <div class="flex flex-col">
+        <Button
+          class="!rounded-none !bg-[#222222] border-none !text-white hover:!text-yellow-300 !text-2xl"
+          on:click={async () => await updateHandler()}
+          {disabled}>Update</Button
+        >
+      </div>
     {:else}
       <p>You're Up to Date!</p>
     {/if}
