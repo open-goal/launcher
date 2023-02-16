2c2a2144c332d515d8986d787cdefb6af4b0e999
Tyler Wilding <xtvaser@gmail.com>
2023-02-16 02:09:05 -0500

    frontend: mostly finish version management page

--- src/routes/settings/Versions.svelte
+++ src/routes/settings/Versions.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { Alert, Button, Tabs, TabItem, Radio } from "flowbite-svelte";
   import {
     Table,
@@ -8,71 +8,288 @@
     TableHead,
     TableHeadCell,
   } from "flowbite-svelte";
+  import Icon from "@iconify/svelte";
+  import { onMount } from "svelte";
+  import { each } from "svelte/internal";
+  import { downloadOfficialVersion, listDownloadedOfficialVersions } from "$lib/rpc/versions";
 
-  let officialVersion = "v0.1.31";
+  let componentLoaded = false;
+  let currentOfficialVersion = "v0.1.31";
+  let selectedOfficialVersion = "v0.1.31";
+
+  const tabItemActiveClasses =
+    "inline-block text-sm font-bold text-center disabled:cursor-not-allowed p-4 text-orange-500 border-b-2 border-orange-500 dark:text-orange-500 dark:border-orange-500";
+  const tabItemInactiveClasses =
+    "inline-block text-sm font-normal text-center disabled:cursor-not-allowed p-4 border-b-2 border-transparent text-gray-400 hover:text-orange-300 hover:border-orange-500 dark:hover:text-orange-300 dark:text-orange-400";
+
+  interface Release {
+    version: string;
+    date: string | undefined;
+    githubLink: string | undefined;
+    downloadUrl: string | undefined; // TODO - windows/mac/linux
+    isDownloaded: boolean;
+    isActive: boolean;
+  }
+
+  let officialReleases: Release[] = [];
+
+  onMount(async () => {
+    await refreshOfficialVersionList(undefined);
+    // TODO - spinner
+    componentLoaded = true;
+  });
+
+  async function refreshOfficialVersionList(evt) {
+    // Check the backend to see if the folder has any versions
+    const installedVersions = await listDownloadedOfficialVersions();
+    officialReleases = [];
+    for (const version of installedVersions) {
+      officialReleases = [
+        ...officialReleases,
+        {
+          version: version,
+          date: undefined,
+          githubLink: undefined,
+          downloadUrl: undefined,
+          isDownloaded: true,
+          isActive: true, // TODO
+        },
+      ];
+    }
+    // TODO - confirmation on deletion / tauri command to handle that
+    // TODO - "no releases found"
+
+    // Merge that with the actual current releases on github
+    // TODO - handle rate limiting
+    // TODO - long term - handle pagination (more than 100 releases)
+    // TODO - even longer term - extract this out into an API we control (avoid github rate limiting) -- will be needed for unofficial releases as well anyway
+    const resp = await fetch(
+      "https://api.github.com/repos/open-goal/jak-project/releases?per_page=100"
+    );
+    // TODO - handle error
+    const githubReleases = await resp.json();
+    for (const release of githubReleases) {
+      // Look to see if we already have this release downloaded and we just have to fill in some metadata about it
+      let foundExistingRelease = false;
+      for (const existingRelease of officialReleases) {
+        if (existingRelease.version == release.tag_name) {
+          existingRelease.date = release.published_at;
+          existingRelease.githubLink = release.html_url;
+          existingRelease.downloadUrl = "https://github.com/open-goal/jak-project/releases/download/v0.1.32/opengoal-windows-v0.1.32.zip";
+          foundExistingRelease = true;
+          break;
+        }
+      }
+      if (foundExistingRelease) {
+        continue;
+      }
+      officialReleases = [
+        ...officialReleases,
+        {
+          version: release.tag_name,
+          date: release.published_at,
+          githubLink: release.html_url,
+          downloadUrl: "https://github.com/open-goal/jak-project/releases/download/v0.1.32/opengoal-windows-v0.1.32.zip",
+          isDownloaded: false,
+          isActive: false, // TODO
+        },
+      ];
+    }
+
+    // Sort releases by published date
+    officialReleases.sort((a, b) => b.date.localeCompare(a.date));
+  }
+
+  async function saveOfficialVersionChange(evt) {
+    // TODO - tauri side
+
+  }
+
+  async function openOfficialVersionFolder(evt) {
+    // TODO - tauri side
+  }
+
+  async function onDownloadOfficialVersion(version: String, url: String) {
+    await downloadOfficialVersion(version, url);
+  }
 </script>
 
-<Tabs style="pill">
-  <TabItem open>
-    <span slot="title">Official</span>
-    <Table>
-      <TableHead>
-        <TableHeadCell class="sr-only" />
-        <TableHeadCell>Version Number</TableHeadCell>
-        <TableHeadCell>Date</TableHeadCell>
-        <TableHeadCell>Github Link</TableHeadCell>
-        <TableHeadCell>
-          <span class="sr-only"> Download </span>
-        </TableHeadCell>
-      </TableHead>
-      <TableBody class="divide-y">
-        <TableBodyRow>
-          <TableBodyCell class="!p-4">
-            <Radio group={officialVersion} value="v0.1.31" />
-          </TableBodyCell>
-          <TableBodyCell>v0.1.31</TableBodyCell>
-          <TableBodyCell>NOW</TableBodyCell>
-          <TableBodyCell>TODO</TableBodyCell>
-          <TableBodyCell>
-            <a
-              href="/tables"
-              class="font-medium text-blue-600 hover:underline dark:text-blue-500"
-            >
-              Download
-            </a>
-          </TableBodyCell>
-        </TableBodyRow>
-        <TableBodyRow>
-          <TableBodyCell class="!p-4">
-            <Radio group={officialVersion} value="v0.1.30" disabled />
-          </TableBodyCell>
-          <TableBodyCell>v0.1.30</TableBodyCell>
-          <TableBodyCell>NOW</TableBodyCell>
-          <TableBodyCell>TODO</TableBodyCell>
-          <TableBodyCell>
-            <a
-              href="/tables"
-              class="font-medium text-blue-600 hover:underline dark:text-blue-500"
-            >
-              Download
-            </a>
-          </TableBodyCell>
-        </TableBodyRow>
-      </TableBody>
-    </Table>
-  </TabItem>
-  <TabItem>
-    <span slot="title">Unofficial</span>
-    <p class="text-sm text-gray-500 dark:text-gray-400">
-      <b>Dashboard:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    </p>
-  </TabItem>
-  <TabItem>
-    <span slot="title">Development</span>
-    <p class="text-sm text-gray-500 dark:text-gray-400">
-      <b>Settings:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    </p>
-  </TabItem>
-</Tabs>
+<!-- TODO - clean up duplication here with some components for each page -->
+
+<div>
+  <p class="text-sm mt-1 mb-1">
+    Configure your active <strong>tooling</strong> version
+  </p>
+  <Tabs style="pill" contentClass="p-4 rounded-lg mt-0 pb-20 overflow-y-auto">
+    <TabItem
+      open
+      activeClasses={tabItemActiveClasses}
+      inactiveClasses={tabItemInactiveClasses}
+    >
+      <span slot="title">Official</span>
+      <div class="flex items-center mb-2">
+        <div class="grow">
+          <p class="text-sm text-gray-400 dark:text-gray-300">
+            Official versions are from the `jak-project` GitHub repository
+          </p>
+        </div>
+        <div class="flex">
+          {#if currentOfficialVersion != selectedOfficialVersion}
+          <Button class="!p-2 mr-2 dark:bg-green-500 hover:dark:bg-green-600" on:click={saveOfficialVersionChange}>
+            <Icon
+              icon="material-symbols:save"
+              width="20"
+              height="20"
+              alt="save official version change"
+            />
+          </Button>
+          {/if}
+          <Button class="!p-2 mr-2" on:click={refreshOfficialVersionList}>
+            <Icon
+              icon="material-symbols:refresh"
+              width="20"
+              height="20"
+              alt="refresh official version list"
+            />
+          </Button>
+          <Button class="!p-2">
+            <Icon
+              icon="material-symbols:folder-open-rounded"
+              width="20"
+              height="20"
+              alt="open official version folder"
+            />
+          </Button>
+        </div>
+      </div>
+      <Table>
+        <TableHead>
+          <TableHeadCell>
+            <span class="sr-only">Select</span>
+          </TableHeadCell>
+          <TableHeadCell>
+            <span class="sr-only">Controls</span>
+          </TableHeadCell>
+          <TableHeadCell>Version</TableHeadCell>
+          <TableHeadCell>Date</TableHeadCell>
+          <TableHeadCell>Github Link</TableHeadCell>
+        </TableHead>
+        <TableBody class="divide-y">
+          {#each officialReleases as release}
+            <TableBodyRow>
+              <TableBodyCell class="!p-4 py-0">
+                <Radio
+                  class="disabled:cursor-not-allowed"
+                  bind:group={selectedOfficialVersion}
+                  value={release.version}
+                  disabled={!release.isDownloaded}
+                  name="official-release"
+                />
+              </TableBodyCell>
+              <TableBodyCell
+                tdClass="px-6 py-0 whitespace-nowrap font-medium"
+                style="line-height: 0;"
+              >
+                <Button class="dark:bg-transparent hover:dark:bg-transparent focus:ring-0 focus:ring-offset-0" on:click={() => onDownloadOfficialVersion(release.version, release.downloadUrl)}>
+                  {#if release.isDownloaded}
+                    <Icon
+                      icon="ic:baseline-delete-forever"
+                      width="24"
+                      height="24"
+                      color="red"
+                    />
+                  {:else}
+                    <Icon icon="ic:baseline-download" color="#00d500" width="24" height="24" />
+                  {/if}
+                </Button>
+              </TableBodyCell>
+              <TableBodyCell tdClass="px-6 py-0 whitespace-nowrap font-medium"
+                >{release.version}</TableBodyCell
+              >
+              <TableBodyCell tdClass="px-6 py-0 whitespace-nowrap font-medium"
+                >{new Date(release.date).toLocaleDateString()}</TableBodyCell
+              >
+              <TableBodyCell tdClass="px-6 py-0 whitespace-nowrap font-medium"
+                ><a href={release.githubLink} target="_blank" rel="noreferrer"
+                  ><Icon icon="mdi:github" width="24" height="24" /></a
+                >
+              </TableBodyCell>
+            </TableBodyRow>
+          {/each}
+        </TableBody>
+      </Table>
+    </TabItem>
+    <TabItem
+      activeClasses={tabItemActiveClasses}
+      inactiveClasses={tabItemInactiveClasses}
+    >
+      <span slot="title">Unofficial</span>
+      <p class="text-sm text-gray-400 dark:text-gray-300">
+        Unofficial versions are typically modified `jak-project` releases to
+        enable changes or new content
+      </p>
+      <div class="flex items-center mt-2 mb-2">
+        <div class="grow">
+          <p class="text-sm text-gray-400 dark:text-gray-300">
+            These are not supported by the OpenGOAL team and will have to be
+            manually added to the folder at this time
+          </p>
+        </div>
+        <div class="flex">
+          <Button class="!p-2 mr-2 dark:bg-orange-600 dark:hover:bg-orange-500">
+            <Icon
+              icon="material-symbols:refresh"
+              width="20"
+              height="20"
+              alt="refresh official version list"
+            />
+          </Button>
+          <Button class="!p-2 dark:bg-orange-600 dark:hover:bg-orange-500">
+            <Icon
+              icon="material-symbols:folder-open-rounded"
+              width="20"
+              height="20"
+              alt="open official version folder"
+            />
+          </Button>
+        </div>
+      </div>
+    </TabItem>
+    <TabItem
+      activeClasses={tabItemActiveClasses}
+      inactiveClasses={tabItemInactiveClasses}
+    >
+      <span slot="title">Development</span>
+      <p class="text-sm text-gray-400 dark:text-gray-300">
+        This list serves as a convenient area to stage, manage and test new
+        releases (either official or unofficial)
+      </p>
+      <div class="flex items-center mt-2 mb-2">
+        <div class="grow">
+          <p class="text-sm text-gray-400 dark:text-gray-300">
+            This list will always require manual management via it's respective
+            folder
+          </p>
+        </div>
+        <div class="flex">
+          <Button class="!p-2 mr-2 dark:bg-orange-600 dark:hover:bg-orange-500">
+            <Icon
+              icon="material-symbols:refresh"
+              width="20"
+              height="20"
+              alt="refresh official version list"
+            />
+          </Button>
+          <Button class="!p-2 dark:bg-orange-600 dark:hover:bg-orange-500">
+            <Icon
+              icon="material-symbols:folder-open-rounded"
+              width="20"
+              height="20"
+              alt="open official version folder"
+            />
+          </Button>
+        </div>
+      </div>
+    </TabItem>
+  </Tabs>
+</div>
