45c5a60a5405fa8b142880c1bc62c07b92627565
Tyler Wilding <xtvaser@gmail.com>
2023-05-06 12:39:38 -0400

    i18n: fix initial load of locales for splash

--- src/splash/splash.ts
+++ src/splash/splash.ts
@@ -2,13 +2,11 @@ import { initLocales } from "$lib/i18n/i18n";
 import App from "./Splash.svelte";
 import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 
-polyfillCountryFlagEmojis();
-
 // Register Translations
-await initLocales(false);
-
-const app = new App({
-  target: document.getElementById("app"),
-});
-
-export default app;
+export default (async () => {
+  polyfillCountryFlagEmojis();
+  await initLocales(false);
+  return new App({
+    target: document.getElementById("app"),
+  });
+})();
