d5bb3f1c59b961833076692c63cb04de66ab3d9f
Tyler Wilding <xtvaser@gmail.com>
2024-03-06 18:46:58 -0500

    ux: get rid of "unexpected error" after first time install

--- src-tauri/src/commands/config.rs
+++ src-tauri/src/commands/config.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use crate::{config::LauncherConfig, util::file::delete_dir};
+use log::log;
 use semver::Version;
 use sysinfo::Disks;
 use tauri::Manager;
@@ -440,19 +441,21 @@ pub async fn does_active_tooling_version_support_game(
   game_name: String,
 ) -> Result<bool, CommandError> {
   let config_lock = config.lock().await;
-  // If we can't determine the version, assume its our first release
-  let active_version = config_lock
-    .active_version
-    .as_ref()
-    .ok_or(CommandError::Configuration(
-      "No active version set, can't perform operation".to_owned(),
-    ))?;
-  let tooling_version = Version::parse(active_version.strip_prefix('v').unwrap_or(&active_version))
-    .unwrap_or(Version::new(0, 0, 1));
-  match game_name.as_str() {
-    "jak1" => Ok(true),
-    "jak2" => Ok(tooling_version.minor > 1 || tooling_version.patch >= 44),
-    _ => Ok(false),
+  match &config_lock.active_version {
+      Some(version) => {
+        // If we can't determine the version, assume its our first release
+        let tooling_version = Version::parse(version.strip_prefix('v').unwrap_or(&version))
+        .unwrap_or(Version::new(0, 0, 1));
+        match game_name.as_str() {
+        "jak1" => Ok(true),
+        "jak2" => Ok(tooling_version.minor > 1 || tooling_version.patch >= 44),
+        _ => Ok(false),
+        }
+      }
+      None => {
+        log::warn!("No active tooling version set, can't check the game supports it!");
+        Ok(false)
+      }
   }
 }
 
