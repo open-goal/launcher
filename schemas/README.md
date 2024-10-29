f91a7deea32943baa97ea0a24abab251599bb261
Tyler Wilding <xtvaser@gmail.com>
2024-10-28 20:41:39 -0400

    schemas: move mod-related schemas to this repository

---
+++ schemas/README.md
@@ -0,0 +1,7 @@
+Generated via typescript definitions:
+
+```bash
+npx ts-json-schema-generator --path './*.ts' --type 'ModMetadata' > mod-schema.v1.json
+```
+
+And then some value validations added on mostly for valid semver checking
---
+++ schemas/mod-source/v1/types.ts
@@ -0,0 +1,42 @@
+/**
+ * Semantic Version
+ * @pattern ^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$
+ */
+type Semver = string;
+
+type SupportedGame = "jak1" | "jak2" | "jak3" | "jakx";
+
+interface ModVersion {
+  version: string;
+  publishedDate: string;
+  assets: Record<string, string | null>;
+  supportedGames?: SupportedGame[];
+}
+
+interface ModPerGameConfig {
+  coverArtUrl?: string;
+  thumbnailArtUrl?: string;
+  releaseDate?: string;
+}
+
+interface ModInfo {
+  displayName: string;
+  description: string;
+  authors: string[];
+  tags: string[];
+  supportedGames: SupportedGame[];
+  websiteUrl?: string;
+  versions: ModVersion[];
+  perGameConfig?: Record<string, ModPerGameConfig>;
+  coverArtUrl?: string;
+  thumbnailArtUrl?: string;
+  externalLink?: string;
+}
+
+export interface ModSourceData {
+  schemaVersion: Semver;
+  sourceName: string;
+  lastUpdated: string;
+  mods: Record<string, ModInfo>
+  texturePacks: Record<string, ModInfo>
+}
