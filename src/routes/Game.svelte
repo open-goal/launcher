2f4dad1dcafd1b3d60c312b898b64ccfe7127c22
Matt Dallmeyer <mattdallmeyer@gmail.com>
2025-06-02 09:05:15 -0700

    Restore beta warning for Jak 2. Use different text/links for Jak 3 beta warning (#764)
    Think there are still some remaining items for Jak 2 before we want to
    consider it out of beta (see discord discussion)

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -48,7 +48,6 @@
   export let game_name;
   $: activeGame.set(game_name);
 
-  $: gameInBeta = $activeGame === SupportedGame.Jak3;
   let gameSupportedByTooling = false;
   let showVccWarning;
   $: showVccWarning = type() == "windows" && !$isMinVCCRuntime;
@@ -160,9 +159,42 @@
         </ul>
       </Alert>
     {/if}
-    {#if gameInBeta}
+
+    <!-- Jak 2 BETA warning -->
+    {#if $activeGame === SupportedGame.Jak2}
       <Alert rounded={false} class="border-t-4 text-red-400">
         <span class="font-bold">{$_("gameControls_beta_headerA")}</span>
+        <em>{$_("gameControls_beta_headerB")}</em>
+        <br />
+        <ul>
+          <li>
+            {$_("gameControls_beta_issueTracker_linkPreText")}
+            <a
+              class="text-blue-400"
+              href="https://github.com/open-goal/jak-project/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc+label%3Ajak2"
+              target="_blank"
+              rel="noopener noreferrer"
+              >{$_("gameControls_beta_issueTracker_linkText")}</a
+            >
+          </li>
+          <li>
+            {$_("gameControls_beta_bugReport_linkPreText")}
+            <a
+              class="text-blue-400"
+              href="https://github.com/open-goal/jak-project/issues/new?template=jak2-bug-report.yml"
+              target="_blank"
+              rel="noopener noreferrer"
+              >{$_("gameControls_beta_bugReport_linkText")}</a
+            >
+          </li>
+        </ul>
+      </Alert>
+    {/if}
+
+    <!-- Jak 3 BETA warning -->
+    {#if $activeGame === SupportedGame.Jak3}
+      <Alert rounded={false} class="border-t-4 text-red-400">
+        <span class="font-bold">{$_("gameControls_beta_headerA_jak3")}</span>
         <em>{$_("gameControls_beta_headerB")}</em>
         <br />
         <ul>
