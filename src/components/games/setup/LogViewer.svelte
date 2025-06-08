15656fef0876c0a88029e4aa73c01487f2b4c297
tripp <86533397+trippjoe@users.noreply.github.com>
2025-06-08 13:48:43 -0400

    fix: multiple logs because i removed unlistener a few months back

--- src/components/games/setup/LogViewer.svelte
+++ src/components/games/setup/LogViewer.svelte
@@ -3,24 +3,41 @@
   import { listen } from "@tauri-apps/api/event";
   import { ansiSpan } from "ansi-to-span";
   import escapeHtml from "escape-html";
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from "svelte";
   import { _ } from "svelte-i18n";
 
+  let logContainer;
+  let unlisten: () => void;
+
   onMount(async () => {
-    await listen("log_update", (event) => {
+    const unlistenFn = await listen("log_update", (event) => {
       const newLogs = event.payload.logs
         .split("\n")
         .map((log) => ansiSpan(escapeHtml(log)).replaceAll("\n", "<br/>"))
         .filter((log) => log.length > 0);
       progressTracker.appendLogs(newLogs);
+
+      requestAnimationFrame(() => {
+        if (logContainer) {
+          logContainer.scrollTop = logContainer.scrollHeight;
+        }
+      });
     });
+    unlisten = unlistenFn;
+  });
+
+  onDestroy(() => {
+    if (unlisten) unlisten();
   });
 </script>
 
 {#if $progressTracker.logs.length > 0}
-  <pre
-    class="rounded bg-[#141414] text-[11px] max-h-[300px] overflow-auto font-mono">{#each $progressTracker.logs as log}
-      {@html log}
+  <div
+    bind:this={logContainer}
+    class="rounded bg-[#141414] text-[11px] max-h-[300px] overflow-auto font-mono px-2 py-2"
+  >
+    {#each $progressTracker.logs as log}
+      <div>{@html log}</div>
     {/each}
-</pre>
+  </div>
 {/if}
