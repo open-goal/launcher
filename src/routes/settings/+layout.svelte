df374543cfe83092aed520c1666dff51b3f6570a
tripp <86533397+trippjoe@users.noreply.github.com>
2025-08-30 17:04:00 -0400

    use flowbite navbar

--- src/routes/settings/+layout.svelte
+++ src/routes/settings/+layout.svelte
@@ -2,19 +2,24 @@
   import type { LayoutProps } from "./$types";
   import { _ } from "svelte-i18n";
   import { page } from "$app/state";
+  import { Navbar, NavLi, NavUl } from "flowbite-svelte";
 
   let { data, children }: LayoutProps = $props();
   let activeUrl = $derived(page.url.pathname);
 </script>
 
 <div class="flex flex-col h-full bg-zinc-900 p-4">
-  <nav class="flex h-10 mx-auto bg-zinc-900">
-    <div class="flex gap-8">
-      <a href="/settings/general">General</a>
-      <a href="/settings/versions">Versions</a>
-      <a href="/settings/decompiler">Decompiler</a>
-      <a href="/settings/mods">Mods</a>
-    </div>
-  </nav>
+  <Navbar class="flex bg-zinc-900 items-center p-0 pb-4">
+    <NavUl
+      {activeUrl}
+      class="min-w-full"
+      ulClass="flex min-w-max justify-between p-0"
+    >
+      <NavLi href="/settings/general">General</NavLi>
+      <NavLi href="/settings/versions">Versions</NavLi>
+      <NavLi href="/settings/decompiler">Decompiler</NavLi>
+      <NavLi href="/settings/mods">Mods</NavLi>
+    </NavUl>
+  </Navbar>
   {@render children()}
 </div>
