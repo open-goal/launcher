7270fcdbe9c2aaa3d702fdace38b81ed6e7ff951
tripp <86533397+trippjoe@users.noreply.github.com>
2025-08-30 21:08:22 -0400

    rm update store

--- src/lib/stores/AppStore.ts
+++ src/lib/stores/AppStore.ts
@@ -2,13 +2,6 @@ import type { SupportedGame } from "$lib/rpc/bindings/SupportedGame";
 import type { ModInfo } from "$lib/rpc/bindings/ModInfo";
 import { writable } from "svelte/store";
 
-export const UpdateStore = writable({
-  selectedTooling: {
-    updateAvailable: false,
-    versionNumber: undefined,
-  },
-});
-
 export const activeGame = writable<SupportedGame>(undefined);
 
 export const modInfoStore = writable<ModInfo | undefined>(undefined);
