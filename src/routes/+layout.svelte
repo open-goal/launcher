f34189bb13ab24cf06fc5d9e164520c9a6170670
tripp <86533397+trippjoe@users.noreply.github.com>
2025-09-01 19:04:52 -0400

    migrate default right click prevention

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,12 +8,42 @@
   import { listen } from "@tauri-apps/api/event";
   import type { LauncherConfig } from "$lib/rpc/bindings/LauncherConfig";
   import { launcherConfig } from "$lib/stores/Config";
+  import { isInDebugMode } from "$lib/utils/common";
 
   let { data, children }: LayoutProps = $props();
+  let revokeSpecificActions = false;
 
   listen<LauncherConfig>("settingsUpdated", (e) => {
     // launcherConfig.set(e.payload);
   });
+
+  if (!isInDebugMode()) {
+    revokeSpecificActions = true;
+    // Disable Right Click
+    document.addEventListener("contextmenu", (event) => {
+      if (revokeSpecificActions) {
+        event.preventDefault();
+      }
+    });
+    // Disable Refreshing (F5 / Ctrl+R)
+    document.addEventListener("keydown", (e) => {
+      if (e.code == "F5") {
+        if (revokeSpecificActions) {
+          e.preventDefault();
+        }
+      }
+      if (e.code == "KeyR" && e.ctrlKey) {
+        if (revokeSpecificActions) {
+          e.preventDefault();
+        }
+      }
+      // super secret keybind to reverse the above so we can debug a release version
+      // Shift+Ctrl F12
+      if (e.code == "F12" && e.ctrlKey && e.shiftKey) {
+        revokeSpecificActions = false;
+      }
+    });
+  }
 </script>
 
 <div class="flex h-screen flex-col">
