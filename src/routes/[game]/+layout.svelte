91f0633435178bdb3c6fccc4ef8d9d2aecaff8eb
tripp <86533397+trippjoe@users.noreply.github.com>
2025-09-01 18:50:43 -0400

    copy some existing html from old game route

--- src/routes/[game]/+layout.svelte
+++ src/routes/[game]/+layout.svelte
@@ -21,3 +21,61 @@
 {:else}
   {@render children()}
 {/if}
+
+<!-- Jak 2 BETA warning -->
+<!-- <Alert rounded={false} class="border-t-4 text-red-400">
+  <span class="font-bold">{$_("gameControls_beta_headerA")}</span>
+  <em>{$_("gameControls_beta_headerB")}</em>
+  <br />
+  <ul>
+    <li>
+      {$_("gameControls_beta_issueTracker_linkPreText")}
+      <a
+        class="text-blue-400"
+        href="https://github.com/open-goal/jak-project/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc+label%3Ajak2"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{$_("gameControls_beta_issueTracker_linkText")}</a
+      >
+    </li>
+    <li>
+      {$_("gameControls_beta_bugReport_linkPreText")}
+      <a
+        class="text-blue-400"
+        href="https://github.com/open-goal/jak-project/issues/new?template=jak2-bug-report.yml"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{$_("gameControls_beta_bugReport_linkText")}</a
+      >
+    </li>
+  </ul>
+</Alert> -->
+
+<!-- Jak 3 BETA warning -->
+<!-- <Alert rounded={false} class="border-t-4 text-red-400">
+  <span class="font-bold">{$_("gameControls_beta_headerA_jak3")}</span>
+  <em>{$_("gameControls_beta_headerB")}</em>
+  <br />
+  <ul>
+    <li>
+      {$_("gameControls_beta_issueTracker_linkPreText")}
+      <a
+        class="text-blue-400"
+        href="https://github.com/open-goal/jak-project/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc+label%3Ajak3"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{$_("gameControls_beta_issueTracker_linkText")}</a
+      >
+    </li>
+    <li>
+      {$_("gameControls_beta_bugReport_linkPreText")}
+      <a
+        class="text-blue-400"
+        href="https://github.com/open-goal/jak-project/issues/new?template=jak3-bug-report.yml"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{$_("gameControls_beta_bugReport_linkText")}</a
+      >
+    </li>
+  </ul>
+</Alert> -->
