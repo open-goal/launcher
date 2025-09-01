9709ccf57f2faeac2536a339c6ce3f7c1e806a08
tripp <86533397+trippjoe@users.noreply.github.com>
2025-09-01 18:51:39 -0400

    preliminary implementation to automatically download the latest tooling version if no tooling version set

--- src/routes/[game]/+layout.ts
+++ src/routes/[game]/+layout.ts
@@ -1,7 +1,22 @@
 import type { LayoutLoad } from "./$types";
+import { saveActiveVersionChange } from "$lib/rpc/config";
+import { downloadLatestOfficialVersion } from "$lib/rpc/versions";
+import { getLatestOfficialRelease } from "$lib/utils/github";
+import { invalidateAll } from "$app/navigation";
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params, parent }) => {
   const game = params.game;
+  const { config } = await parent();
+
+  // INSTALL LATEST TOOLING VERSION IF NONE SET
+  // needs some work to be fully functional, need a global store for if were already downloading
+  // if (!config.activeVersion) {
+  //   console.log("downloading the latest release");
+  //   const release = await getLatestOfficialRelease();
+  //   await downloadLatestOfficialVersion();
+  //   await saveActiveVersionChange(release);
+  //   await invalidateAll();
+  // }
 
   return { game };
 }) satisfies LayoutLoad;
