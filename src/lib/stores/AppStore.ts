10dc5e410a7ec515667080805ce0391f0b038de2
tripp <86533397+trippjoe@users.noreply.github.com>
2025-09-02 16:41:35 -0400

    nuke old appstore

--- src/lib/stores/AppStore.ts
+++
@@ -1,7 +0,0 @@
-import type { SupportedGame } from "$lib/rpc/bindings/SupportedGame";
-import type { ModInfo } from "$lib/rpc/bindings/ModInfo";
-import { writable } from "svelte/store";
-
-export const activeGame = writable<SupportedGame>(undefined);
-
-export const modInfoStore = writable<ModInfo | undefined>(undefined);
