d4ffcdb37f2d0941c9a4d7673576582618f7ced6
tripp <86533397+trippjoe@users.noreply.github.com>
2025-09-02 16:46:20 -0400

    nuke old version store

--- src/lib/stores/VersionStore.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-
-export interface VersionStoreIFace {
-  activeVersionName: string | null;
-}
-
-export const VersionStore = writable<VersionStoreIFace>({
-  activeVersionName: null,
-});
