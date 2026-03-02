4695dc9750b945079b869bd23f4ad79de765ad85
tripp <86533397+trippjoe@users.noreply.github.com>
2026-03-02 14:43:14 -0500

    refactor: add anyhow context and use AsRef<Path> in tar extraction helpers

--- src-tauri/src/util/tar.rs
+++ src-tauri/src/util/tar.rs
@@ -1,20 +1,31 @@
-use std::path::PathBuf;
+use anyhow::{Context, Result};
+use std::path::Path;
 
-pub fn extract_tar_ball(tar_path: &PathBuf, extract_dir: &PathBuf) -> Result<(), std::io::Error> {
-  log::info!("extracting {}", tar_path.display());
-  let tar_gz = std::fs::File::open(tar_path)?;
+pub fn extract_tar_ball(tar_path: impl AsRef<Path>, extract_dir: impl AsRef<Path>) -> Result<()> {
+  let tar_path = tar_path.as_ref();
+  let extract_dir = extract_dir.as_ref();
+  let tar_gz = std::fs::File::open(tar_path)
+    .with_context(|| format!("failed to open: {}", tar_path.display()))?;
   let tar = flate2::read::GzDecoder::new(tar_gz);
   let mut archive = tar::Archive::new(tar);
-  archive.unpack(extract_dir)?;
+  archive.unpack(extract_dir).with_context(|| {
+    format!(
+      "failed to unpack: {} into {}",
+      tar_path.display(),
+      extract_dir.display()
+    )
+  })?;
   Ok(())
 }
 
 pub fn extract_and_delete_tar_ball(
-  tar_path: &PathBuf,
-  extract_dir: &PathBuf,
-) -> Result<(), std::io::Error> {
+  tar_path: impl AsRef<Path>,
+  extract_dir: impl AsRef<Path>,
+) -> Result<()> {
+  let tar_path = tar_path.as_ref();
+  let extract_dir = extract_dir.as_ref();
   extract_tar_ball(tar_path, extract_dir)?;
-  log::info!("deleting {}", tar_path.display());
-  std::fs::remove_file(tar_path)?;
+  std::fs::remove_file(tar_path)
+    .with_context(|| format!("failed to delete: {}", tar_path.display()))?;
   Ok(())
 }
