27a729d5597dae34ddcc27dc5902b26ccabfa3ef
tripp <86533397+trippjoe@users.noreply.github.com>
2026-03-02 19:49:38 -0500

    refactor: download_json helper, rm: unused error enum type

--- src-tauri/src/util/network.rs
+++ src-tauri/src/util/network.rs
@@ -3,16 +3,6 @@ use std::path::Path;
 use tokio;
 use tokio::io::AsyncWriteExt;
 
-#[derive(Debug, thiserror::Error)]
-pub enum NetworkError {
-  #[error(transparent)]
-  IO(#[from] std::io::Error),
-  #[error(transparent)]
-  NetworkRequest(#[from] reqwest::Error),
-  #[error("{0}")]
-  Message(String),
-}
-
 pub async fn download_file(url: &str, destination: impl AsRef<Path>) -> Result<()> {
   let destination = destination.as_ref();
   let mut response = reqwest::get(url)
@@ -37,13 +27,15 @@ pub async fn download_file(url: &str, destination: impl AsRef<Path>) -> Result<(
   Ok(())
 }
 
-pub async fn download_json(url: &str) -> Result<String, NetworkError> {
-  let res = reqwest::get(url).await?;
-  if res.status().is_success() {
-    return Ok(res.text().await?);
-  }
-  Err(NetworkError::Message(format!(
-    "Unable to download json from {url}, status: {}",
-    res.status()
-  )))
+pub async fn download_json(url: &str) -> Result<String> {
+  let res = reqwest::get(url)
+    .await
+    .with_context(|| format!("Failed to download json from {url}"))?
+    .error_for_status()
+    .with_context(|| format!("Server returned error status for {url}"))?;
+
+  res
+    .text()
+    .await
+    .with_context(|| format!("Failed to read response body from {url}"))
 }
