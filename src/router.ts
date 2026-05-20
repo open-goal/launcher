895597f7f8fccdf58fe1b294f8ea10467402441d
tripp <86533397+trippjoe@users.noreply.github.com>
2026-05-20 18:59:27 -0400

    reorganize texture-pack components

--- src/router.ts
+++ src/router.ts
@@ -8,7 +8,7 @@ import Help from "./routes/Help.svelte";
 // @ts-ignore
 import Mods from "./routes/Mods.svelte";
 // @ts-ignore
-import TexturePacks from "./components/games/features/texture-packs/TexturePacks.svelte";
+import TexturePacks from "./components/texture-packs/TexturePacks.svelte";
 // @ts-ignore
 import UpdateLauncher from "./routes/UpdateLauncher.svelte";
 // @ts-ignore
