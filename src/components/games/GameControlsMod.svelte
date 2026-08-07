a6177611666d5e3faab0cee4b9c4b3ad1a5d141a
tripp <86533397+trippjoe@users.noreply.github.com>
2026-08-07 13:58:45 -0400

    temp: comment out textures button

--- src/components/games/GameControlsMod.svelte
+++ src/components/games/GameControlsMod.svelte
@@ -310,7 +310,8 @@
           }}>{$_("gameControls_update_mod")}</Button
         >
       {/if}
-      <Button
+      <!-- TODO: Uncomment after I finish mods texture support -->
+      <!-- <Button
         onclick={async () => {
           navigate(`/:game_name/mods/:source_name/:mod_name/texture_packs`, {
             params: {
@@ -322,7 +323,7 @@
         }}
         class="font-medium text-gray-200 h-10 text-center focus:ring-0 focus:outline-none border-solid border border-[#2a2a2a] rounded bg-[#0b0b0b] hover:bg-[#141414] hover:border-[#3a3a3a] hover:text-white"
         >{$_("gameControls_button_features_textures")}
-      </Button>
+      </Button> -->
       {#if modVersionListSorted.length > 0}
         <Button
           class="relative font-medium text-gray-200 h-10 text-center focus:ring-0 focus:outline-none border-solid border border-[#2a2a2a] rounded bg-[#0b0b0b] hover:bg-[#141414] hover:border-[#3a3a3a] hover:text-white"
